d3d5e2256a9d6340313d3e71275381c077a06004
Eman Nabi <f223626@cfd.nu.edu.pk>
2024-10-05 15:15:17 +0500

    Added Main class to test Library functions

--- src/Library.java
+++ src/Library.java
@@ -1,15 +1,24 @@
 import java.util.ArrayList;
-   import java.util.List;
+import java.util.List;
 
-   public class Library {
-       private List<String> books = new ArrayList<>();
+public class Library {
+	private List<String> books = new ArrayList<>();
 
-       public void addBook(String bookName) {
-           books.add(bookName);
-           System.out.println(bookName + " added to library.");
-       }
+	public void addBook(String bookName) {
+		books.add(bookName);
+		System.out.println(bookName + " added to library.");
+	}
 
-       public void listBooks() {
-           System.out.println("Books in library: " + books);
-       }   }
- 
+	public void listBooks() {
+		System.out.println("Books in library: " + books);
+	}
+}
+
+public class Main {
+	public static void main(String[] args) {
+		Library myLibrary = new Library();
+		myLibrary.addBook("Harry Potter");
+		myLibrary.addBook("Lord of the Rings");
+		myLibrary.listBooks();
+	}
+}
